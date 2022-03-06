29fe97e84377c2c6322b80e11774c064d0180b3b
Busari Ibtisan <wwwbit2001@yahoo.fr>
2022-03-06 11:59:14 +0000

    correctiontoken

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const auth_token = process.env.JWT_PASSWORD
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, auth_token);
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
     const userId = decodedToken.userId;
     req.auth = { userId };  
     if (req.body.userId && req.body.userId !== userId) {
